549a004448655c97d759abfb9e2b7c7a4c94029a
yucong <yucong@yucong-Inspiron-537s.(none)>
2013-11-26 23:20:47 -0700

    final compiled

---
+++ plan_launch.sh
@@ -0,0 +1,20 @@
+gnome-terminal \
+       	--tab --title "roscore" --command "bash -c \"
+       			                roscore; 
+                                        exec bash\""  \
+       --tab --title "ardrone_driver" --command "bash -c \"
+                                        env sleep 1s ;
+                                        rosrun ardrone_autonomy ardrone_driver _realtime_navdata:=True _navdata_demo:=1; 
+                                        exec bash\""  \
+	--tab --title "joystick" --command "bash -c \"
+                                        env sleep 3s ;
+                                        rosrun joy joy_node;
+                                        exec bash\""  \
+	--tab --title "planner"	 --command "bash -c \"
+					env sleep 3s;
+					roslaunch ardrone_rrt_avoid replan.launch; 
+					exec bash\""  \
+	--tab --title "exe_path" --command "bash -c \"
+                                        env sleep 3s ;
+                                        rosrun ardrone_rrt_avoid exe_path;
+                                        exec bash\""  \
