1ebb0533db920b09caf27f26b78dbf77a48e4f85
yucong <yucong@yucong-Inspiron-570.(none)>
2013-12-13 16:32:33 -0700

    add sh files

---
+++ ardroneA_launch.sh
@@ -0,0 +1,20 @@
+gnome-terminal \
+	--tab --title "roscore" --command "bash -c \"
+				roscore; 
+				exec bash\""  \
+	--tab --title "ardrone_driver" --command "bash -c \"
+				env sleep 1s ;
+				rosrun ardrone_autonomy ardrone_driver _realtime_navdata:=True _navdata_demo:=1; 
+				exec bash\""  \
+	--tab --title "joystick" --command "bash -c \"
+				env sleep 3s ;
+				rosrun joy joy_node;
+				exec bash\""  \
+	--tab --title "planner" --command "bash -c \"
+	                        env sleep 3s;
+	                        roslaunch ardrone_rrt_avoid parrot_plan.launch >rec_plan.txt; 
+	                        exec bash\""  \
+	--tab --title "exe_host" --command "bash -c \"
+	                        env sleep 3s ;
+	                        rosrun ardrone_rrt_avoid exe_host >rec_exeA.txt;
+	                        exec bash\""  \
---
+++ ardroneB_launch.sh
@@ -0,0 +1,16 @@
+gnome-terminal \
+	--tab --title "roscore" --command "bash -c \"
+				roscore; 
+				exec bash\""  \
+	--tab --title "ardrone_driver" --command "bash -c \"
+				env sleep 1s ;
+				rosrun ardrone_autonomy ardrone_driver _realtime_navdata:=True _navdata_demo:=1; 
+				exec bash\""  \
+	--tab --title "joystick" --command "bash -c \"
+				env sleep 3s ;
+				rosrun joy joy_node;
+				exec bash\""  \
+	--tab --title "exe_intruder" --command "bash -c \"
+				env sleep 3s ;
+				rosrun ardrone_rrt_avoid exe_intruder >rec_exeB.txt;
+				exec bash\""  \
