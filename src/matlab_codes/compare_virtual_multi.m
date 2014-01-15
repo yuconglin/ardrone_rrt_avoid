833cf9a9c824df13a0debb42b6e8b339614d59a8
yucong <yucong@yucong-Inspiron-570.(none)>
2014-01-15 04:24:33 -0700

    updated some matlab codes

---
+++ src/matlab_codes/compare_virtual_multi.m
@@ -0,0 +1,117 @@
+close all;
+clear all;
+exe_files=[
+'20140101-224638:path.txt'
+'20140101-225051:path.txt'
+'20140101-225407:path.txt'
+'20140101-225638:path.txt'
+'20140102-053627:path.txt'
+];
+virtual_files={
+      %'v1.0_the0.0.txt';
+      %'v-1.0_t5.0.txt';
+      'v1.0_the0.0.txt'
+    };
+%virtual_char= char(virtual_files);
+min_dis= zeros( size(virtual_files,1), size(exe_files,1) );
+
+for l=1:size(exe_files,1)
+%for l=1:1   
+    close all;
+    %load the file
+    file_path= sprintf('%s%s','../../data/',exe_files(l,:) );
+    log_data =fopen(file_path,'r');
+    if log_data == -1
+         error('File log_data could not be opened, check name or path.')
+    end
+    log_line= fgetl(log_data);
+    reg = [];
+    if_log= 0;
+    pre_state= -1;
+
+    while ischar(log_line)
+       log_reg = textscan(log_line,'%f %f %f %f %f %f %f %f %f %f');
+       t = log_reg{1};
+       x = log_reg{2};
+       y = log_reg{3};
+       z = log_reg{4};
+       vx = log_reg{6};
+       vy = log_reg{7};
+       vz = log_reg{8};
+       vw = log_reg{9};
+       yaw = log_reg{5};
+       state = log_reg{10};
+
+       log_line= fgetl(log_data);
+
+       if(pre_state~= 4 && state== 4)
+          if_log= 1;
+          t0=t;
+       end
+
+       pre_state= state;
+
+       if(if_log== 1 && (state== 3 || state== 7) )
+          t= t-t0;
+          reg= [reg; [t,vx,vy,vz,vw,yaw,state,x,y,z] ]; 
+       end
+
+    end
+
+    for j=1:size(min_dis,1)
+        obs_rec= sprintf('%s%s','../../virtual_obs/',char(virtual_files(j) ) );
+        log_data =fopen(obs_rec,'r');
+        if log_data == -1
+           error('File obs_rec.txt could not be opened, check name or path.' ); 
+        end
+        obs_line= fgetl(log_data);
+        obs = [];
+        while ischar(obs_line)
+           log_obs = textscan(obs_line,'%f %f %f %f %f %f %f %f');
+           t = log_obs{1};
+           if t> 0
+             if size(obs,1)== 0
+                t0 = log_obs{1};
+             end
+             vx = log_obs{2};
+             vy = log_obs{3};
+             vz = log_obs{4};
+             w = log_obs{5};
+             x = log_obs{6};
+             y = log_obs{7};
+             z = log_obs{8};
+             obs = [ obs; [t-t0,vx,vy,vz,w,x,y,z] ];
+           end%if ends
+           obs_line= fgetl(log_data);
+        end
+
+        obs_match = [];
+        dis_rec = [];
+        %quad and obstacle time matching
+        idx = 1;
+        for i=1:size(reg,1)
+            t= reg(i,1);
+
+            if( t< obs(1,1) )
+              obs_match = [obs_match; obs(1,:)];
+            elseif( t> obs( size(obs,1),1) )
+              obs_match = [obs_match; obs(size(obs,1),:) ];
+            else
+              while(1)
+                 if (idx+1<= size(obs,1) && obs(idx,1)<=t && t<= obs(idx+1,1)) || (idx+1 == size(obs,1) )
+                    break; 
+                 else
+                   idx = idx +1;
+                 end
+              end %while ends
+              %idx,t
+              obs_match= [obs_match; obs(idx,:)];
+            end
+
+            dis = sqrt( (obs(idx,6)-reg(i,8))^2+(obs(idx,7)-reg(i,9))^2 +(obs(idx,8)-reg(i,10))^2 );
+            dis_rec = [dis_rec; dis];
+        end
+        min_dis(j,l)= min(dis_rec);
+    end
+    
+end
--- src/matlab_codes/draw_compare_virtual.m
+++ src/matlab_codes/draw_compare_virtual.m
@@ -1,10 +1,6 @@
 close all;
 %load the file
-<<<<<<< HEAD
-log_data =fopen('../../data/20140102-001503:path.txt','r');
-=======
-log_data =fopen('../../data/20140102-222404:path.txt','r');
->>>>>>> a77b4e94a11f486e5e45af949ee80fa3f48cc3e9
+log_data =fopen('../../data/20140101-224638:path.txt','r');
 if log_data == -1
      error('File log_data could not be opened, check name or path.')
 end
@@ -31,7 +27,7 @@ while ischar(log_line)
    
    if(pre_state~= 4 && state== 4)
       if_log= 1;
-      t=0;
+      t0=t;
    end
 %    if(t<10) 
 %      if_log= 1;
@@ -40,18 +36,14 @@ while ischar(log_line)
    pre_state= state;
    
    if(if_log== 1 && (state== 3 || state== 7) )
-      %t= t-t0;
+      t= t-t0;
       reg= [reg; [t,vx,vy,vz,vw,yaw,state,x,y,z] ]; 
    end
    
 end
 
 %logged virtual file
-<<<<<<< HEAD
-obs_rec =fopen('../../virtual_obs/v-1.0_t7.0.txt','r');
-=======
-obs_rec =fopen('../../virtual_obs/static1.txt','r');
->>>>>>> a77b4e94a11f486e5e45af949ee80fa3f48cc3e9
+obs_rec =fopen('../../virtual_obs/v1.0_the0.0.txt','r');
 if obs_rec == -1
     error('File obs_rec.txt could not be opened, check name or path.' ); 
 end
@@ -102,7 +94,59 @@ for i=1:size(reg,1)
     dis = sqrt( (obs(idx,6)-reg(i,8))^2+(obs(idx,7)-reg(i,9))^2 +(obs(idx,8)-reg(i,10))^2 );
     dis_rec = [dis_rec; dis];
 end
-min(dis_rec)
+%min(dis_rec);
+%logged virtual file
+% obs_rec =fopen('../../virtual_obs/v-1.0_t5.0.txt','r');
+% if obs_rec == -1
+%     error('File obs_rec.txt could not be opened, check name or path.' ); 
+% end
+% obs_line= fgetl(obs_rec);
+% obs = [];
+% while ischar(obs_line)
+%    log_obs = textscan(obs_line,'%f %f %f %f %f %f %f %f');
+%    t = log_obs{1};
+%    if t> 0
+%      if size(obs,1)== 0
+%         t0 = log_obs{1};
+%      end
+%      vx = log_obs{2};
+%      vy = log_obs{3};
+%      vz = log_obs{4};
+%      w = log_obs{5};
+%      x = log_obs{6};
+%      y = log_obs{7};
+%      z = log_obs{8};
+%      obs = [ obs; [t-t0,vx,vy,vz,w,x,y,z] ];
+%    end%if ends
+%    obs_line= fgetl(obs_rec);
+% end
+% 
+% obs_match1 = [];
+% dis_rec = [];
+% %quad and obstacle time matching
+% idx = 1;
+% for i=1:size(reg,1)
+%     t= reg(i,1);
+%     
+%     if( t< obs(1,1) )
+%       obs_match1 = [obs_match1; obs(1,:)];
+%     elseif( t> obs( size(obs,1),1) )
+%       obs_match1 = [obs_match1; obs(size(obs,1),:) ];
+%     else
+%       while(1)
+%          if (idx+1<= size(obs,1) && obs(idx,1)<=t && t<= obs(idx+1,1)) || (idx+1 == size(obs,1) )
+%             break; 
+%          else
+%            idx = idx +1;
+%          end
+%       end %while ends
+%       %idx,t
+%       obs_match1= [obs_match1; obs(idx,:)];
+%     end
+%     
+%     dis = sqrt( (obs(idx,6)-reg(i,8))^2+(obs(idx,7)-reg(i,9))^2 +(obs(idx,8)-reg(i,10))^2 );
+%     dis_rec = [dis_rec; dis];
+% end
 
 %drawing
 figure;
@@ -110,19 +154,30 @@ hold on;
 axis auto;
 grid on;
 title('plot');
-xlabel('Easting(km)');
-ylabel('Northing(km)');
-zlabel('Altitude(ft)');
+xlabel('x(m)');
+ylabel('y(m)');
+zlabel('z(m)');
+axis([ 0, 15, -5, 5, -1, 3]);
 %unit sphere
 r = 1.5;
 [x,y,z] = sphere;
-hSphere = surf( r*x+obs_match(1,6),r*y+obs_match(1,7),r*z+obs_match(1,8) );
+hSphere = surf( r*x+obs_match(1,6),r*y+obs_match(1,7),r*z+obs_match(1,8),'FaceColor','b' );
+%hSphere1= surf( r*x+obs_match1(1,6),r*y+obs_match1(1,7),r*z+obs_match1(1,8),'FaceColor','g' );
 %draw every point
+k=1;
 for i=1:size(reg,1)
+%for i=1;10*k
     %pause(0.1);
     plot3( reg(i,8),reg(i,9),reg(i,10), 'r*' );
     delete(hSphere);
-    hSphere = surf( r*x+obs_match(i,6),r*y+obs_match(i,7),r*z+obs_match(i,8) );
+    hSphere = surf( r*x+obs_match(i,6),r*y+obs_match(i,7),r*z+obs_match(i,8),'FaceColor','b' );
+    %delete(hSphere1);
+    %hSphere1= surf( r*x+obs_match1(i,6),r*y+obs_match1(i,7),r*z+obs_match1(i,8),'FaceColor','g' );
     view(3);
     drawnow;
+%     if mod(i,10)==0
+%       filename= sprintf('/home/yucong/Dropbox/iros_icuas/iros2014/figs/matlab_figs/virtual%d.fig',k);
+%       saveas(gcf,filename);
+%       k=k+1;
+%     end
 end
